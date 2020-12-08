28b9d6c8b8b0b74a7b59aa043aee41cccc720a99
TanyaKomyakova <tanya_komyakova@mail.ru>
2020-12-08 16:08:32 +0300

    launch SUT

--- src/test/java/ru/netology/web/test/AuthorizationTest.java
+++ src/test/java/ru/netology/web/test/AuthorizationTest.java
@@ -1,2 +1,58 @@
-package ru.netology.web.test;public class AuthorizationTest {
-}
+package ru.netology.web.test;
+
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+import com.github.javafaker.Faker;
+import lombok.val;
+import org.apache.commons.dbutils.QueryRunner;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AuthorizationTest {
+    @AfterAll
+    void setUp() throws SQLException {
+        val faker = new Faker();
+        val runner = new QueryRunner();
+        val dataSQL = "INSERT INTO users(login, password) VALUES (?, ?);";
+
+        try (
+                val conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+                );
+
+        ) {
+            runner.update(conn, dataSQL, faker.name().username(), "pass");
+            runner.update(conn, dataSQL, faker.name().username(), "pass");
+        }
+    }
+    @Test
+    void stubTest() throws SQLException {
+        val countSQL = "SELECT COUNT(*) FROM auth_codes;";
+        val runner = new QueryRunner();
+
+        try (
+                val conn = DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/app", "app", "pass"
+                );
+        )
+
+
+
+    @Test
+
+    void autor(){
+        open("http://localhost:9999");
+        SelenideElement form = $("form");
+        form.$("[data-test-id=login] input").setValue("vasya");
+        form.$("[data-test-id=password] input").setValue("qwerty123");
+
+
+
+    }
